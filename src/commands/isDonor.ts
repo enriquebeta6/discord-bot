33979b1a44cbf7f017cfc1625275aee767c03f75
Enrique Mora <enriquebeta7@gmail.com>
2021-12-20 09:16:35 -0300

    feat: add isDonor command

---
+++ src/commands/isDonor.ts
@@ -0,0 +1,29 @@
+// Dependencies
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+// Typings
+import { CommandInteractionWithClientWithCommands } from '../typings/discord.d';
+
+// Services
+import { toysLegendDonation } from '../services/toysLegendDonation';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('isdonor')
+    .setDescription('Verify if the wallet is a donor!')
+    .addStringOption((option) =>
+      option
+        .setName('address')
+        .setDescription('The wallet address to verify')
+        .setRequired(true)
+    ),
+  async execute(interaction: CommandInteractionWithClientWithCommands) {
+    const address = interaction.options.getString('address');
+
+    const result = await toysLegendDonation.isDonator(address);
+
+    const replyMessage = result ? `You're a donor` : `You aren't a donor`;
+
+    await interaction.reply(replyMessage);
+  },
+};
