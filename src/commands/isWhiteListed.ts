92c25cdbc4cc2a79c1b5ecb124eff2658d24f470
Enrique Mora <enriquebeta7@gmail.com>
2021-12-20 09:17:21 -0300

    feat: add iswhitelisted command

---
+++ src/commands/isWhiteListed.ts
@@ -0,0 +1,30 @@
+// Dependencies
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+// Typings
+import { CommandInteractionWithClientWithCommands } from '../typings/discord.d';
+
+// Services
+import { toysLegendWhiteList } from '../services/toysLegendWhiteList';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('iswhitelisted')
+    .setDescription('Verify if the wallet is whitelisted!')
+    .addStringOption((option) =>
+      option
+        .setName('address')
+        .setDescription('The wallet address to verify')
+        .setRequired(true)
+    ),
+  async execute(interaction: CommandInteractionWithClientWithCommands) {
+    const address = interaction.options.getString('address');
+    const result = await toysLegendWhiteList.whitelist(address);
+
+    const replyMessage = result
+      ? `You're whitelisted`
+      : `You aren't whitelisted`;
+
+    await interaction.reply(replyMessage);
+  },
+};
